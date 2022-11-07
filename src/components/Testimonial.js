dca804d8085c59a00aca562fa18d2438d8285618
sebastianAksamitowski <s.aksamitowski@gmail.com>
2022-11-07 23:16:15 +0100

    initial commit

---
+++ src/components/Testimonial.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import TestimonialCarousel from '../components/TestimonialCarousel';
+
+import { testimonialData } from '../data';
+
+// import motion
+import { motion } from 'framer-motion';
+// import variants
+import { fadeIn } from '../variants';
+
+const Testimonial = () => {
+  const { title, subtitle, modelImg, slider } = testimonialData;
+  return (
+    <section className='bg-testimonial bg-cover bg-no-repeat relative top-[340px] lg:top-[390px] z-10 h-[880px] pt-[60px] md:pt-[120px]'>
+      <div className='container mx-auto'>
+        {/* text */}
+        <motion.div
+          variants={fadeIn('up', 'tween', 0.2, 1.6)}
+          initial='hidden'
+          whileInView={'show'}
+          className='text-center capitalize flex flex-col items-center'
+        >
+          <h2 className='h2 text-white'>{title}</h2>
+          <p className='text-white/70 capitalize mb-8'>{subtitle}</p>
+          <div className='mb-12'>
+            <img src={modelImg} />
+          </div>
+        </motion.div>
+        {/* slider */}
+        <motion.div
+          variants={fadeIn('up', 'tween', 0.4, 1.6)}
+          initial='hidden'
+          whileInView={'show'}
+          className='flex justify-center items-center'
+        >
+          <TestimonialCarousel slider={slider} />
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default Testimonial;
